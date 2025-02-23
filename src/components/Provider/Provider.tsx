9d006c0b04c652f1deea16dbf53a6baefdbfb576
nisalk <nirvanzentinal@gmail.com>
2025-02-23 19:18:50 +0530

    dark mode removed

--- src/components/Provider/Provider.tsx
+++ src/components/Provider/Provider.tsx
@@ -2,14 +2,11 @@
 
 import { ReactNode } from "react";
 import NextTopLoader from "nextjs-toploader";
-import { ThemeProvider } from "next-themes";
 function Provider({ children }: { children: ReactNode }) {
   return (
     <div>
-      <ThemeProvider>
-        <NextTopLoader color="#4ade80" />
-        {children}
-      </ThemeProvider>
+      <NextTopLoader color="#4ade80" />
+      {children}
     </div>
   );
 }
