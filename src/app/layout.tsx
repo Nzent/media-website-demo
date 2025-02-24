cecb9b9b21f3879f60dc4ea1e5e607378c2843ce
nisalk <nirvanzentinal@gmail.com>
2025-02-24 22:04:54 +0530

    og image added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,37 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Excellence Media",
   description: "Sri Lanka no 1 Media and Advertisement Agency",
+
+  authors: [{ name: "Nisalk" }],
+  creator: "Nisalk ",
+  publisher: "Nisalk ",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "hhttps://excellence-demo.vercel.app/",
+    title: "Excellence - Professional medial agency",
+    description:
+      "Discover Excellence, professional team of media and advertisement agency",
+    siteName: "Excellence",
+    images: [
+      {
+        url: "/images/excellence-og.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Excellence - Professional medial agency",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    site: "@Excellence",
+    creator: "@Excellence",
+    title: "Excellence - Professional medial agency",
+    description:
+      "Discover Excellence, professional team of media and advertisement agency",
+    images: ["/images/excellence-og.jpg"],
+  },
 };
 
 export default function RootLayout({
